c00beb60168391527d8d1d2a0ed0886d076eb401
bhoomi252 <143258144+bhoomi252@users.noreply.github.com>
2023-11-19 14:33:08 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,20 @@
-print ("Hello");
+print ("Enter 2 number to perform calculation: ");
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+calc = input("Enter the operator: ")
+if calc=="+":
+  print("The sum of",num1,"&",num2,"is:",num1+num2 )
+elif calc=="-":
+  print("The subtraction of",num1,"&",num2,"is:",num1-num2)
+elif calc=="*":
+  print("The multiplication of",num1,"&",num2,"is:",num1*num2)
+elif calc=="/":
+  print("The division of",num1,"&",num2,"is:",num1/num2)
+elif calc=="**":
+  print("The exponention of",num1,"&",num2,"is:",num1**num2)
+elif calc=="%":
+  print("The modulus of",num1,"&",num2,"is:",num1%num2)
+elif calc=="//":
+  print("The floor division of",num1,"&",num2,"is:",num1//num2)
+else:
+  print("Invalid")

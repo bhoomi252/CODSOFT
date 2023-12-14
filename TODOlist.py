a6a7ff7a4494b679c8f3a0fa95b947a7f8da57bf
Bhoomika Khapare <143258144+bhoomi252@users.noreply.github.com>
2023-12-14 14:21:05 +0530

    TODO list

--- TODOlist.py
+++ TODOlist.py
@@ -5,21 +5,22 @@ class Todo(tk.Tk):
         super().__init__()
         self.geometry("500x500")
         self.title("TO DO LIST")
+        self.configure(bg='light green')
         self.create_widgets()
  
     def create_widgets(self):
-        self.task_input = tk.Entry(self, width=40)
-        self.task_input.pack(pady=10)
-        self.add_button = tk.Button(self, text="Add your task", command=self.add_task)
+        self.task_input = tk.Entry(self, width=40, font=("arial", 14))
+        self.task_input.pack(pady=10,padx=10)
+        self.add_button = tk.Button(self, text="Add your task", command=self.add_task,width=15, font=("arial", 12,'bold'), border=5)
         self.add_button.pack(pady=5)
-        self.tasks_box = tk.Listbox(self, selectmode=tk.SINGLE)
+        self.tasks_box = tk.Listbox(self, selectmode=tk.SINGLE, font=("arial", 14))
         self.tasks_box.pack(pady=5)
         self.button_frame = tk.Frame(self)
         self.button_frame.pack(pady=5)
-        self.edit_button = tk.Button(self.button_frame, text="Edit the task", command=self.edit_task) 
-        self.edit_button.grid(row=0, column=0, padx=5)
-        self.delete_button = tk.Button(self.button_frame, text="Delete the task", command=self.delete_task)
-        self.delete_button.grid(row=0, column=1, padx=5)
+        self.edit_button = tk.Button(self.button_frame, text="Edit the task", command=self.edit_task, width=15, font=("arial", 12,'bold'), border=5) 
+        self.edit_button.grid(row=1, column=1)
+        self.delete_button = tk.Button(self.button_frame, text="Delete the task", command=self.delete_task,width=15, font=("arial", 12,'bold'), border=5)
+        self.delete_button.grid(row=3, column=1, pady=2)
     
     def add_task(self):
         task = self.task_input.get()
@@ -43,4 +44,4 @@ class Todo(tk.Tk):
  
 if __name__ == "__main__":
     app = Todo()
-    app.mainloop()
+    app.mainloop() 
